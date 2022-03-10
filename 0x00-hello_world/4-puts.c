1801ba9015671641df913644608a1309203d045c
nziza-saint <nzizacooling@gmail.com>
2022-03-10 01:33:43 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - printing the word in puts
+ * Return - 0
+ */
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
   return (0);
 }
+
