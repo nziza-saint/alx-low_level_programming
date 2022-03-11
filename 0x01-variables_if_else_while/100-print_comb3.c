d18a06e76b7d3b4bfc60102e36a52a8e7a4b2e3b
nziza-saint <nzizacooling@gmail.com>
2022-03-11 12:56:24 -0800

    add 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,26 +1,23 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible different combinations of two digits
- * Return: ALways 0 (Success)
+ * main - prints the numbers from 00 to 99
+ * Return: Always 0 (success)
  */
 int main(void)
 {
 int n, m;
 
-for (n = 48; n <= 56; n++)
+for (n = 48; n <= 57; n++)
 {
-for (m = 49; m <= 57; m++)
+for (m = 48; m <= 57; m++)
 {
-if (m > n)
-{
-putchar(n)
+putchar(n);
 putchar(m);
-if (n != 56 || m != 57)
+if (n != 57 || m != 57)
 {
 putchar(',');
 putchar(' ');
-}
 }
 }
 }
